5dcc80880cf9a25fab2ba504a97cbe1cc0f4a529
MarkChang <markchang@markchang-desktop.(none)>
2013-02-07 01:54:32 +0800

    add draw dot

--- CameraTest/gen/com/ntugiee/markchang/cameratest/R.java
+++ CameraTest/gen/com/ntugiee/markchang/cameratest/R.java
@@ -31,7 +31,8 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int editimage=0x7f030001;
-        public static final int uploadimage=0x7f030002;
+        public static final int editpanel=0x7f030002;
+        public static final int uploadimage=0x7f030003;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
--- CameraTest/src/com/ntugiee/markchang/cameratest/EditImageActivity.java
+++ CameraTest/src/com/ntugiee/markchang/cameratest/EditImageActivity.java
@@ -2,6 +2,7 @@ package com.ntugiee.markchang.cameratest;
 
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -18,87 +19,49 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class EditImageActivity extends Activity {
-	
-	private Button b1;
-	private Button b2;
-	private Button b3;
-
-	private TextView tv;
-	private ImageView ivEdit;
-	private Bitmap bitmap;
-	private Canvas canvas;
-	private Paint paint ;
-	private int fieldImgXY[] = new int[2];
-
-	@Override
+    private float x=0;
+    private float y=0;
+    private Button b1;
+    public OnTouchListener panel_on_touch;
+    /** Called when the activity is first created. */
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.editimage);
-        Intent intent = this.getIntent();
-        Bundle bundle = intent.getExtras();
-        String encodedString = bundle.getString("img");
+        setContentView(R.layout.editpanel);
+        b1 = new Button(this);
+        b1.setText("Button1");
+        
+        MyCustomPanel view = new MyCustomPanel(EditImageActivity.this);
 
-        ivEdit = (ImageView)findViewById(R.id.ivEdit);
-	    byte [] encodeByte=Base64.decode(encodedString,Base64.DEFAULT);
-	    bitmap=BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
-	    
-	    ivEdit.getLocationInWindow(fieldImgXY);
+        
 
-	    ivEdit.setImageBitmap(bitmap);
-	    //Region region = new Region(ivEdit.getLeft(), ivEdit.getTop(), ivEdit.getRight(), ivEdit.getBottom());
-	    
-	    Log.d("position",String.valueOf(ivEdit.getLeft())+" "+String.valueOf(ivEdit.getTop())+ " "
-	    		+String.valueOf(ivEdit.getHeight())+" "+String.valueOf(ivEdit.getWidth())
-	    		);
-	    Log.d( "fieldImage lockation on screen: ", 
-	    		String.valueOf(fieldImgXY[0])+" "+String.valueOf(fieldImgXY[1]));    
-	    
-        b1 = (Button)findViewById(R.id.EditButton1);
-        b2 = (Button)findViewById(R.id.EditButton2);
-        b3 = (Button)findViewById(R.id.EditButton3);
+                
 
-        canvas = new Canvas();
-        canvas.drawColor(Color.TRANSPARENT);
-        paint = new Paint();
-        paint.setColor(Color.BLACK);
-        paint.setStyle(Paint.Style.FILL);
+        addContentView(view, new ViewGroup.LayoutParams(LayoutParams.FILL_PARENT,
+                LayoutParams.FILL_PARENT));
+        
+        addContentView(b1, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
         
         
-        b1.setOnClickListener(new View.OnClickListener() {
-        	public void onClick(View view) {
-                    
-        	}
-        } );     
-        
-        ivEdit.setOnTouchListener(new OnTouchListener() {
+        panel_on_touch= new OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 //Choose which motion action has been performed
                 switch(event.getAction())
                 {
                 case MotionEvent.ACTION_DOWN:
-                    //Get X, Y coordinates from the ImageView
-                    int X = (int) event.getX();
-                    int Y = (int) event.getY();
-            	    Log.d("position",String.valueOf(X)+" "+String.valueOf(Y));
-            	    
-            	    //canvas.drawBitmap(bitmap, X, Y, paint);
-                    //ivEdit.setImageBitmap(bitmap);
-                    /*float[] pts={50,50,  
-                            400,50,  
-                            400,600,  
-                            60,600, };                      //数据  
-                    canvas.drawColor(Color.BLACK);                  //白色背景  
-                    paint.setStrokeWidth((float) 20.0);         //线宽  
-                    canvas.drawPoints(pts,paint);                   //绘制点  */
-            	    
-                    //Do something
+                    x = event.getX();
+                    y = event.getY();                 
+                    v.invalidate();
                     break;
                 case MotionEvent.ACTION_MOVE:
                     break;
@@ -107,6 +70,25 @@ public class EditImageActivity extends Activity {
                 }
                 return true;
             }
-        }); 
-	}
+        };
+        view.setOnTouchListener(panel_on_touch);
+        
+        b1.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {    
+				Log.d("click","click");
+			}
+		});
+
+    }
+    private class MyCustomPanel extends View {
+        public MyCustomPanel(Context context) {
+            super(context);
+        }
+        @Override
+        public void draw(Canvas canvas) {
+            Paint paint = new Paint();
+            canvas.drawCircle(x,y,3,paint);
+        }
+    }
+
 }
